216a19228f7b565e4260f72e6f7156f102123466
Jad El Nader <Jadelnader99@gmail.com>
2024-02-26 19:38:32 +0100

    different functions for angular/Typescript

---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+export class SinglefruitComponent {
+@Input()fruit = {
+  name: "Appel",
+  img:"",
+  description: "",
+  genus: "",
+  stars: 0,
+  reviews:[{name: "", text: ""},{name: "", text: ""}],
+};
+
+inputData = "";
+
+@Output()fruitname = new EventEmitter<string>()
+
+sendInputData(){
+ this.fruitname.emit(this.inputData);
+ this.inputData = "";
+}
+
+}
